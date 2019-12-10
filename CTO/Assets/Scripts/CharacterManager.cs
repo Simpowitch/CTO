a113e8662c845f13cbbb07b80e66e089fb869dab
Simon Voss <55984457+Simwpowitch@users.noreply.github.com>
2019-12-10 19:01:53 +0100

    Squares editors + simple test ai

---
+++ CTO/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    #region Singleton
+    public static CharacterManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogError("Instance of " + this + " was tried to be instantiated again");
+        }
+    }
+    #endregion
+
+    private static Character selectedCharacter;
+    public static Character SelectedCharacter
+    {
+        get
+        {
+            return selectedCharacter;
+        }
+        set
+        {
+            if (selectedCharacter)
+            {
+                //selectedCharacter.gameObject.GetComponent<MeshRenderer>().material = GridSystem.instance.defaultSquareMaterial;
+            }
+            selectedCharacter = value;
+            //selectedCharacter.gameObject.GetComponent<MeshRenderer>().material = GridSystem.instance.selectedSquareMaterial;
+        }
+    }
+}
---
+++ CTO/Assets/Scripts/Editor/GridSystemEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GridSystem))]
+public class GridSystemEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GridSystem system = (GridSystem)target;
+
+        //DrawDefaultInspector();
+
+        GUILayout.Space(10);
+        GUILayout.Label("Grid Editor", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Create Squares"))
+        {
+            system.SpawnSquares();
+        }
+
+        if (GUILayout.Button("Delete Squares"))
+        {
+            system.DeleteAllSquares();
+        }
+    }
+}
