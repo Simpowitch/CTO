7fa4d60482331cb0397b215ea9dd2946ee6dc3ec
peanutpowah <samuel.zelenka@hotmail.com>
2020-01-21 14:54:48 +0100

    FOW v0.1

---
+++ CTO/Assets/Scripts/FogOfWar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOfWar : MonoBehaviour
+{
+
+    public MeshFilter mesh;
+    List<Vector3> vertices;
+    private void Start()
+    {
+        vertices = new List<Vector3>(mesh.mesh.vertices);
+        List<Vector3> verticesNoDuplicates = new List<Vector3>();
+
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            if (!verticesNoDuplicates.Contains(vertices[i]))
+            {
+                verticesNoDuplicates.Add(vertices[i]);
+            }
+        }
+        vertices = verticesNoDuplicates;
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            Gizmos.DrawSphere(mesh.transform.position + vertices[i], 0.1f);
+            print(i);
+        }
+    }
+}
